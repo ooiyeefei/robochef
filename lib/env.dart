2a86787b7439c4c765f37313435713387beae108
sebastian <leesebas@amazon.com>
2023-10-19 17:54:43 +0800

    changes require to work on local

--- lib/env.dart
+++ lib/env.dart
@@ -2,16 +2,16 @@
 
 import 'package:envied/envied.dart';
 
-part 'env.g.dart';
+// part 'env.g.dart';
 
-@Envied(path: '.env')
-abstract class Env {
-  @EnviedField(varName: 'FIREBASE_WEBAPP_API_KEY', obfuscate: true)
-  static final String firebaseWebAppApiKey = _Env.firebaseWebAppApiKey;
-  @EnviedField(varName: 'AGW_URL', obfuscate: true)
-  static final String agwUrl = _Env.agwUrl;
+// @Envied(path: '.env')
+// abstract class Env {
+//   @EnviedField(varName: 'FIREBASE_WEBAPP_API_KEY', obfuscate: true)
+//   static final String firebaseWebAppApiKey = _Env.firebaseWebAppApiKey;
+//   @EnviedField(varName: 'AGW_URL', obfuscate: true)
+//   static final String agwUrl = _Env.agwUrl;
   // @EnviedField(varName: 'KEY2')
   // static const String key2 = _Env.key2;
   // @EnviedField()
   // static const String key3 = _Env.key3;
-}
+// }
